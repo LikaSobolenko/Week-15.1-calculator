1cd37c536d625de8a28508abef5cce34132ac8fb
LikaSobolenko <lika160892@yandex.ru>
2022-02-17 16:48:40 +0700

    вывод сообщения

--- Week 15.1 calculator/assets/calculator.js
+++ Week 15.1 calculator/assets/calculator.js
@@ -22,10 +22,11 @@ function multiplication() {
 function division() {
     let a = Number(document.getElementById(`number1`).value);
     let b = Number(document.getElementById(`number2`).value);
-    if (b==0) {
-        alert('На ноль делить нельзя :З')
+    if (b == 0) {
+        let result = 'На ноль делить нельзя :З';
+        document.getElementById('result').innerHTML = result;
+    } else {
+        let result = a / b;
+        document.getElementById('result').innerHTML = result;
     }
-    let result = a / b;
-    document.getElementById('result').innerHTML = result;
 }
-
